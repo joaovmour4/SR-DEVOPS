d147ec2ddd6e0630296d76d97c69d1310b102e6d
ramoncbarbosa <ramon.cobarbosa@gmail.com>
2023-11-14 17:13:21 -0300

    subindo uuuiiiiii

--- Front-end/src/componentes/Header.jsx
+++ Front-end/src/componentes/Header.jsx
@@ -2,28 +2,28 @@ import React from "react";
 import imgRU from "../img/RU-por-extenso-b 1.png";
 import imgCardapio from "../img/cardapio.png";
 import imgLogin from "../img/login.png";
+import { Link } from "react-router-dom"
 
-
-export default function Header(){
-    return (
-        <section>
-            <header className="bg-cor-um flex grow-0 justify-center">
-                <img src={imgRU} alt="Imagem do RU da Unifesspa"/>
-            </header>
-            <nav className="bg-cor-dois flex justify-center first-letter gap-x-4 py-2 flex-grow-0">
-                <a className="flex flex-col items-center" href="./cardapio.html">
-                    <img className="h-6 w-6" src={imgCardapio} alt="img do cardapio"/>
-                    <label className="text-sm">
-                        CARDÁPIO
-                    </label>
-                </a>
-                <a className="flex flex-col items-center" href="./login.html">
-                    <img className="h-6 w-6" src={imgLogin} alt="img do login"/>
-                    <label className="bg-gray-300 text-sm">
-                        LOGIN
-                    </label>
-                </a>
-            </nav>
-        </section>
-        )
-} 
+export default function Header() {
+  return (
+    <section>
+      <header className="bg-cor-um flex grow-0 justify-center">
+        <img src={imgRU} alt="Imagem do RU da Unifesspa" />
+      </header>
+      <nav className="bg-cor-dois flex justify-center first-letter gap-x-4 py-2 flex-grow-0">
+        <p className="flex flex-col items-center">
+          <img className="h-6 w-6" src={imgCardapio} alt="img do cardápio" />
+          <label className="text-sm">CARDÁPIO</label>
+        </p>
+        <p className="flex flex-col items-center">
+          <img className="h-6 w-6" src={imgLogin} alt="img do login" /> 
+          <p className="bg-gray-300 text-sm">
+            <a href="https://www.w3schools.com">
+              LOGIN
+            </a>
+          </p>
+        </p>
+      </nav>
+    </section>
+  );
+}
--- Front-end/src/views/Cardapio.jsx
+++ Front-end/src/views/Cardapio.jsx
@@ -1,13 +1,72 @@
 import React from 'react';
-import Header from "../componentes/Header"
-import Footer from "../componentes/Footer";
 
-export default function cardapio(){
+export default function Cardapio(){
     return (
         <>
-            <Footer/>
-
-            <Header/>
+            <main class="cardapio">
+                <div class="container">
+                    <h1>DIA E PRATO</h1>
+                </div>
+                <div class="menu-segunda">
+                    <h2>Segunda-feira</h2>
+                    <ul>
+                        <h4>GUISADO DE CARNE / FRANGO
+                            VEGETARIANO: REFOGADO DE LENTILHA</h4>
+                            <p>ARROZ BRANCO</p>
+                            <p>FEIJÃO COM MACAXEIRA E COUVE</p>
+                            <p>FAROFA</p>
+                            <p>SALADA CRUA CEN/REP</p>
+                            <p>TANGERINA/MELANCIA;</p>
+                    </ul>
+                </div> 
+                <div class="menu-terca">
+                    <h2>Terça-feira</h2>
+                    <ul>
+                        <h4>FEIJOADA
+                            VEGETARIANO: FEIJOADA VEGETARIANA</h4>
+                        <p>ARROZ BRANCO;</p>
+                        <p>FAROFA;</p>
+                        <p>COUVE E REPOLHO REFOGADOS;</p>
+                        <p>TANGERINA/MAÇÃ ;</p>
+                    </ul>
+                </div>
+                <div class="menu-quarta">
+                    <h2>Quarta-feira</h2>
+                    <h4>CARNE ASSADA
+                        VEGETARIANO: ESCONDIDINHO DE SOJA</h4>
+                    <ul>
+                        <p>ARROZ BRANCO;</p>
+                        <p>FEIJÃO COM ABÓBORA E COUVE;</p>
+                        <p>FAROFA;</p>
+                        <p>SALADA AGRIDOCE;</p>
+                        <p>BANANA;</p>
+                    </ul>
+                </div>
+                <div class="menu-quinta">
+                    <h2>Quinta-feira</h2>
+                    <h4>LINGUIÇA AO FORNO
+                        VEGETARIANO: POLENTA VEGETARIANA</h4>
+                    <ul>
+                        <p>ARROZ BRANCO;</p>
+                        <p>FEIJÃO COM VINAGREIRA E INHAME;</p>
+                        <p>FAROFA;</p>
+                        <p>PEPINO AO VINAGRETE;</p>
+                        <p>MANGA;</p>
+                    </ul>
+                </div>
+                <div class="menu-sexta">
+                    <h2>Sexta-feira</h2>
+                    <h4>PICADINHO À JARDINEIRA
+                        VEGETARIANO: QUINOA À JARDINEIRA</h4>
+                    <ul>
+                        <p>ARROZ BRANCO;</p>
+                        <p>FEIJÃO COM MACAXEIRA E CARIRU;</p>
+                        <p>FAROFA;</p>
+                        <p>CENOURA AO VINAGRETE;</p>
+                        <p>TANGERINA;</p>
+                    </ul>
+                </div>
+            </main>
         </>
     )
 }
