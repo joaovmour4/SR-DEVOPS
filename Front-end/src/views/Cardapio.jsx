43fc184fe7f111ecc20dcbc5a16a513c781b486b
ramoncbarbosa <ramon.cobarbosa@gmail.com>
2023-11-20 14:02:41 -0300

    ...

--- Front-end/src/views/Cardapio.jsx
+++ Front-end/src/views/Cardapio.jsx
@@ -1,76 +1,96 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
 
-export default function Cardapio(){
-    return (
-        <>
-        <Link to={'/'}>
-            Home
-        </Link>
-            <main class="cardapio">
-                <div class="container">
-                    <h1>DIA E PRATO</h1>
-                </div>
-                <div class="menu-segunda">
-                    <h2>Segunda-feira</h2>
-                    <ul>
-                        <h4>GUISADO DE CARNE / FRANGO
-                            VEGETARIANO: REFOGADO DE LENTILHA</h4>
-                            <p>ARROZ BRANCO</p>
-                            <p>FEIJÃO COM MACAXEIRA E COUVE</p>
-                            <p>FAROFA</p>
-                            <p>SALADA CRUA CEN/REP</p>
-                            <p>TANGERINA/MELANCIA;</p>
-                    </ul>
-                </div> 
-                <div class="menu-terca">
-                    <h2>Terça-feira</h2>
-                    <ul>
-                        <h4>FEIJOADA
-                            VEGETARIANO: FEIJOADA VEGETARIANA</h4>
-                        <p>ARROZ BRANCO;</p>
-                        <p>FAROFA;</p>
-                        <p>COUVE E REPOLHO REFOGADOS;</p>
-                        <p>TANGERINA/MAÇÃ ;</p>
-                    </ul>
-                </div>
-                <div class="menu-quarta">
-                    <h2>Quarta-feira</h2>
-                    <h4>CARNE ASSADA
-                        VEGETARIANO: ESCONDIDINHO DE SOJA</h4>
-                    <ul>
-                        <p>ARROZ BRANCO;</p>
-                        <p>FEIJÃO COM ABÓBORA E COUVE;</p>
-                        <p>FAROFA;</p>
-                        <p>SALADA AGRIDOCE;</p>
-                        <p>BANANA;</p>
-                    </ul>
-                </div>
-                <div class="menu-quinta">
-                    <h2>Quinta-feira</h2>
-                    <h4>LINGUIÇA AO FORNO
-                        VEGETARIANO: POLENTA VEGETARIANA</h4>
-                    <ul>
-                        <p>ARROZ BRANCO;</p>
-                        <p>FEIJÃO COM VINAGREIRA E INHAME;</p>
-                        <p>FAROFA;</p>
-                        <p>PEPINO AO VINAGRETE;</p>
-                        <p>MANGA;</p>
-                    </ul>
-                </div>
-                <div class="menu-sexta">
-                    <h2>Sexta-feira</h2>
-                    <h4>PICADINHO À JARDINEIRA
-                        VEGETARIANO: QUINOA À JARDINEIRA</h4>
-                    <ul>
-                        <p>ARROZ BRANCO;</p>
-                        <p>FEIJÃO COM MACAXEIRA E CARIRU;</p>
-                        <p>FAROFA;</p>
-                        <p>CENOURA AO VINAGRETE;</p>
-                        <p>TANGERINA;</p>
-                    </ul>
-                </div>
-            </main>
-        </>
-    )
+const cardapioData = {
+    SEGUNDA: {
+      prato: 'GUISADO DE CARNE / FRANGO',
+      vegetariano: 'VEGETARIANO: REFOGADO DE LENTILHA',
+      acompanhamentos: ['ARROZ BRANCO', 'FEIJÃO COM MACAXEIRA E COUVE', 'FAROFA', 'SALADA CRUA CEN/REP', 'TANGERINA/MELANCIA'],
+    },
+    TERÇA: {
+      prato: 'ESCONDIDINHO DE FRANGO',
+      vegetariano: 'VEGETARIANO: PANQUECA DE PALMITO',
+      acompanhamentos: ['ARROZ COM CENOURA E SALSINHA', 'FEIJÃO TROPEIRO', 'FAROFA DE OVOS', 'SALADA DE BETERRABA', 'MAMÃO FORMOSA'],
+    },
+    QUARTA: {
+      prato: 'STROGONOFF DE CARNE',
+      vegetariano: 'VEGETARIANO: RISOTO DE FUNGHI',
+      acompanhamentos: ['ARROZ À GREGA', 'FEIJÃO CARIOCA', 'BATATA PALHA', 'SALADA DE ALFACE E TOMATE', 'MORANGO'],
+    },
+    QUINTA: {
+      prato: 'YAKISSOBA DE FRANGO',
+      vegetariano: 'VEGETARIANO: ABÓBORA REFOGADA',
+      acompanhamentos: ['ARROZ INTEGRAL', 'FEIJÃO PRETO', 'SOPA DE LEGUMES', 'SALADA DE RÚCULA E AGRIÃO', 'ABACAXI'],
+    },
+    SEXTA: {
+      prato: 'PEITO DE FRANGO GRELHADO',
+      vegetariano: 'VEGETARIANO: NHOQUE DE BATATA-DOCE',
+      acompanhamentos: ['ARROZ 7 GRÃOS', 'FEIJÃO BRANCO', 'FAROFA DE AVEIA', 'SALADA DE COUVE-FLOR', 'UVA'],
+    },
+    SÁBADO: {
+      prato: 'LASANHA DE CARNE',
+      vegetariano: 'VEGETARIANO: LASANHA DE BERINJELA',
+      acompanhamentos: ['ARROZ ARBÓREO', 'FEIJÃO ANGELA', 'BATATA GRATINADA', 'SALADA DE AGRIÃO E ALFACE', 'MELÃO'],
+    },
+  };
+  
+
+export default function Cardapio() {
+  const [clickedBlock, setClickedBlock] = useState(null);
+
+  const handleBlockClick = (day) => {
+    console.log(`Clicou em ${day}`);
+    setClickedBlock(day);
+  };
+
+  const handleCloseModal = () => {
+    setClickedBlock(null);
+  };
+
+  return (
+    <div>
+      <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 font-serif p-4">
+        {/* Dia e Prato - ocupando todo o espaço */}
+        <div className="container col-span-full border border-gray-300 rounded p-2 mb-4">
+          <h1 className="text-center">CARDÁPIO SEMANAL</h1>
+        </div>
+
+        {/* Dia e Prato - Segunda a Sábado */}
+        {['SEGUNDA', 'TERÇA', 'QUARTA', 'QUINTA', 'SEXTA', 'SÁBADO'].map((day) => (
+          <div
+            key={day}
+            style={{ transition: 'transform 0.3s', transform: clickedBlock === day ? 'translateY(-10px)' : '' }}
+            className={`border border-gray-300 rounded p-4 md:col-span-1 lg:col-span-1`}
+            onClick={() => handleBlockClick(day)}
+          >
+            <h2 className="text-center">{day}</h2>
+            {/* ... conteúdo do dia */}
+          </div>
+        ))}
+      </main>
+
+      {clickedBlock && (
+  <div className="fixed inset-0 flex items-center justify-center">
+    <div className="absolute inset-0 bg-gray-800 opacity-75" onClick={handleCloseModal}></div>
+    <div className="relative bg-white p-8 rounded-lg text-center">
+      <h2 className="text-center mb-4">{clickedBlock}</h2>
+      <p className="text-left">{cardapioData[clickedBlock].prato}</p>
+      <p className="text-left">{cardapioData[clickedBlock].vegetariano}</p>
+      <ul className="text-left">
+        {cardapioData[clickedBlock].acompanhamentos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <div className="flex justify-between mt-4">
+        <button className="bg-blue-500 text-white py-2 px-4 rounded" onClick={() => console.log('Comprar')}>
+          Comprar
+        </button>
+        <button className="bg-red-500 text-white py-2 px-4 rounded" onClick={handleCloseModal}>
+          Fechar
+        </button>
+      </div>
+    </div>
+  </div>
+)}
+    </div>
+  );
 }
