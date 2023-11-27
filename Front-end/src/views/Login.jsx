70b7511da3ff3c8a38eef9f0ca763ed820841123
ramoncbarbosa <ramon.cobarbosa@gmail.com>
2023-11-27 11:18:00 -0300

    mexendo na tela de login

--- Front-end/src/views/Login.jsx
+++ Front-end/src/views/Login.jsx
@@ -1,57 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import imgAcenando from "../img/aluno.png";
 
 export default function Login() {
-  const navigate = useNavigate();
+  const [loginEfetuado, setLoginEfetuado] = useState(false);
+  const navigate = useNavigate(); 
 
   const realizarLogin = () => {
-    // ainda não sei
-    navigate("/user");
+    setLoginEfetuado(true);
+
+    setTimeout(() => {
+      navigate("/user");
+    }, );
   };
 
   const irCadastro = () => {
     navigate("/cadastro");
-  };
+  }
 
   return (
-    <main className="container flex flex-col justify-center items-center flex-grow min-h-screen w-full mx-auto">
-      <div className="aluno flex flex-col items-center gap-4">
-        <img className="w-32 animate-bounce" src={imgAcenando} alt="GIF acenando" />
-        <form className="form_aluno text-center" onSubmit={(e) => e.preventDefault()}>
-          <input
-            id="user"
-            className="nome bg-gray-300 rounded-md p-2 box-shadow w-64 mb-2 md:mx-4"
-            type="email"
-            name="username"
-            placeholder="E-MAIL"
-            autoComplete="on"
-          />
-          <input
-            id="password"
-            className="senha bg-gray-300 rounded-md p-2 box-shadow w-64 mb-2 md:mx-4"
-            type="password"
-            name="pwd"
-            placeholder="SENHA"
-          />
-          <div className="buttons grid grid-cols-1 gap-4 w-64 mx-auto">
-            <button
-              onClick={realizarLogin}
-              className="entrar_aluno bg-green-500 text-white p-2 hover:bg-green-600"
-              id="entrarAluno"
-            >
-              ENTRAR
-            </button>
-            <button
-              className="cadastroVisitante bg-gray-300 p-2 hover:bg-gray-200"
-              id="cadastroVisitante"
-              onClick={irCadastro}
-            >
-              CADASTRO VISITANTE
-            </button>
+    <>
+     <main className="container flex flex-col justify-center items-center flex-grow min-h-screen w-full mx-auto">
+        {!loginEfetuado ? (
+          <div className="aluno flex flex-col items-center gap-4">
+            <img className="w-32 animate-bounce" src={imgAcenando} alt="GIF acenando" />
+            <form className="form_aluno text-center" onSubmit={(e) => e.preventDefault()}>
+              <input
+                id="user"
+                className="nome bg-gray-300 rounded-md p-2 box-shadow w-64 mb-2 md:mx-4"
+                type="email"
+                name="username"
+                placeholder="E-MAIL"
+                autoComplete="on"
+              />
+              <input
+                id="password"
+                className="senha bg-gray-300 rounded-md p-2 box-shadow w-64 mb-2 md:mx-4"
+                type="password"
+                name="pwd"
+                placeholder="SENHA"
+              />
+              <div className="buttons grid grid-cols-1 gap-4 w-64 mx-auto">
+                <button
+                  onClick={realizarLogin}
+                  className="entrar_aluno bg-green-500 text-white p-2 hover:bg-green-600"
+                  id="entrarAluno"
+                >
+                  ENTRAR
+                </button>
+                <button className="cadastroVisitante bg-gray-300 p-2 hover:bg-gray-200" id="cadastroVisitante" onClick={irCadastro}>
+                  CADASTRO VISITANTE
+                </button>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
-    </main>
+        ) : null}
+      </main>
+    </>
   );
 }
