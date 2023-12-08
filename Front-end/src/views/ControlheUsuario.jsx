b5353ed19fee0ddcc95a3507df92f70138787b87
ramoncbarbosa <ramon.cobarbosa@gmail.com>
2023-12-08 16:54:49 -0300

    tem algum erro para buscar o usuario

--- Front-end/src/views/ControlheUsuario.jsx
+++ Front-end/src/views/ControlheUsuario.jsx
@@ -9,6 +9,7 @@ export default function ControleUsuario() {
   const [isEdicaoAtiva, setEdicaoAtiva] = useState(false);
   const [novoNome, setNovoNome] = useState('');
   const [novaSenha, setNovaSenha] = useState('');
+  const [novoEmail, setNovoEmail] = useState('');
   const [historicoCompras, setHistoricoCompras] = useState([]);
   const [isHistoricoVisivel, setHistoricoVisivel] = useState(false);
 
@@ -19,9 +20,18 @@ export default function ControleUsuario() {
   }, [usuarioEncontrado]);
 
   const handleBuscarUsuario = () => {
-    axios.get(`http://localhost:3000/user/id/${idUsuario}`)
+    axios.get(`http://localhost:3000/user?userName=${idUsuario}`)
       .then(response => {
-        setUsuarioEncontrado(response.data);
+        const foundUser = response.data.message[0];
+        setUsuarioEncontrado({
+          userId: foundUser._id,
+          userName: foundUser.userName,
+          userPassword: foundUser.userPassword,
+          userEmail: foundUser.userEmail,
+          userSubsidio: foundUser.userSubsidio,
+          userCargo: foundUser.userCargo,
+          userPurchases: foundUser.userPurchases,
+        });
         setMostrarInputBusca(false);
         setMostrarBotoes(true);
       })
@@ -35,48 +45,36 @@ export default function ControleUsuario() {
       });
   };
 
-  const handleEditarUsuario = () => {
-    setEdicaoAtiva(true);
-  };
-
-  const handleConfirmarEdicao = () => {
-    axios.put(`http://localhost:3000/user/id/${idUsuario}`, {
+  const handleAlterarDados = () => {
+    // Realizar o PUT na rota http://localhost:3000/user/{id} com os dados atualizados
+    axios.put(`http://localhost:3000/user/${usuarioEncontrado.userId}`, {
       userName: novoNome,
       userPassword: novaSenha,
+      userEmail: novoEmail,
     })
       .then(response => {
-        console.log('Usuário atualizado com sucesso:', response.data);
-        setUsuarioEncontrado(response.data);
+        console.log('Dados do usuário atualizados com sucesso!');
         setEdicaoAtiva(false);
+        setUsuarioEncontrado({
+          ...usuarioEncontrado,
+          userName: novoNome,
+          userEmail: novoEmail,
+        });
       })
       .catch(error => {
-        console.error('Erro ao atualizar o usuário:', error);
+        console.error('Erro ao atualizar os dados do usuário:', error);
       });
-  };
-
-  const handleHistoricoToggle = () => {
-    setHistoricoVisivel(prev => !prev);
-
-    if (!isHistoricoVisivel && usuarioEncontrado) {
-      axios.get(`http://localhost:3000/user/id/${idUsuario}/purchases`)
-        .then(response => {
-          setHistoricoCompras(response.data);
-        })
-        .catch(error => {
-          console.error('Erro ao obter o histórico de compras:', error);
-        });
-    }
   };
 
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="p-4 max-w-md w-full text-center">
         {mostrarInputBusca && (
-          <div>
+          <div className="mb-4">
             <input
               type="text"
-              className="shadow appearance-none border rounded w-full py-2 px-3 mb-4 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="ID do Usuário"
+              className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Digite o nome do usuário"
               value={idUsuario}
               onChange={(e) => setIdUsuario(e.target.value)}
             />
@@ -84,67 +82,55 @@ export default function ControleUsuario() {
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               onClick={handleBuscarUsuario}
             >
-              Buscar Usuário
+              Buscar
             </button>
           </div>
         )}
 
         {mostrarBotoes && (
-          <>
-            <div className="mb-4">
-              <p className="text-gray-700 text-sm font-bold mb-2">Nome do Usuário: {usuarioEncontrado?.userName}</p>
-              {!isEdicaoAtiva && (
-                <button
-                  className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline items-center"
-                  onClick={handleEditarUsuario}
-                >
-                  Editar
-                </button>
-              )}
-            </div>
-
-            {isEdicaoAtiva && (
-              <div className="flex items-center mt-2 justify-center">
-                <input
-                  type="text"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 leading-tight focus:outline-none focus:shadow-outline"
-                  placeholder="Novo Nome"
-                  value={novoNome}
-                  onChange={(e) => setNovoNome(e.target.value)}
-                />
-                <input
-                  type="password"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 leading-tight focus:outline-none focus:shadow-outline"
-                  placeholder="Nova Senha"
-                  value={novaSenha}
-                  onChange={(e) => setNovaSenha(e.target.value)}
-                />
-                <button
-                  className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  onClick={handleConfirmarEdicao}
-                >
-                  Confirmar
-                </button>
-              </div>
-            )}
-
-            <div className="mb-4 mt-4">
+          <div className="mb-4">
+            <p className="text-gray-700 text-sm font-bold mb-2">Nome do Usuário: {usuarioEncontrado?.userName}</p>
+            {!isEdicaoAtiva && (
               <button
-                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                onClick={handleHistoricoToggle}
+                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline items-center"
+                onClick={() => setEdicaoAtiva(true)}
               >
-                {isHistoricoVisivel ? 'Recolher Histórico' : 'Mostrar Histórico'}
+                Editar
               </button>
-            </div>
-
-            {isHistoricoVisivel && (
-              <ul className="list-disc pl-4">
-                {historicoCompras.map((compra, index) => (
-                  <li key={index}>{compra}</li>
-                ))}
-              </ul>
             )}
-          </>
+          </div>
+        )}
+
+        {isEdicaoAtiva && (
+          <div className="flex items-center mt-2 justify-center">
+            <input
+              type="text"
+              className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Novo Nome"
+              value={novoNome}
+              onChange={(e) => setNovoNome(e.target.value)}
+            />
+            <input
+              type="password"
+              className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Nova Senha"
+              value={novaSenha}
+              onChange={(e) => setNovaSenha(e.target.value)}
+            />
+            <input
+              type="text"
+              className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Novo Email"
+              value={novoEmail}
+              onChange={(e) => setNovoEmail(e.target.value)}
+            />
+            <button
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              onClick={handleAlterarDados}
+            >
+              Confirmar
+            </button>
+          </div>
         )}
       </div>
     </div>
