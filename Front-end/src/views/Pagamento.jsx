933a5b62a2967557e448897b39aca97c12655426
ramoncbarbosa <ramon.cobarbosa@gmail.com>
2023-12-18 11:00:09 -0300

    metodo de pagamento implementado

--- Front-end/src/views/Pagamento.jsx
+++ Front-end/src/views/Pagamento.jsx
@@ -1,9 +1,29 @@
-import APIMercadoPago from "../componentes/APIMercadoPago/APIMercadoPago"
+import React, { useState } from 'react';
+import QuantidadePrato from "../componentes/QuantidadePrato/QuantidadePrato";
+import CrediDebCard from "../componentes/CrediDebiCard/CrediDebCard";
 
 export default function Pagamento() {
-    return(
-        <div className='h-screen'>
-            <APIMercadoPago/>
-        </div>
-    )
+  const [quantidadeSelecionada, setQuantidadeSelecionada] = useState(null);
+
+  const handlePurchase = (quantidade) => {
+    setQuantidadeSelecionada(quantidade);
+  };
+
+  const handleIrParaCrediDebCard = () => {
+    // Lógica para ir para o componente CrediDebCard
+    console.log('Indo para CrediDebCard');
+  };
+
+  return (
+    <div className="h-screen w-full mt-16">
+      {quantidadeSelecionada === null ? (
+        <QuantidadePrato
+          onPurchase={handlePurchase}
+          onIrParaCrediDebCard={handleIrParaCrediDebCard}
+        />
+      ) : (
+        <CrediDebCard quantidadeSelecionada={quantidadeSelecionada} />
+      )}
+    </div>
+  );
 }
