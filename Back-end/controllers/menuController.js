476b558aa73138fae621efa040467f5691255267
joaovmour4 <joaov.mour@unifesspa.edu.br>
2023-12-19 00:36:52 -0300

    Rotas de cardápio

--- Back-end/controllers/menuController.js
+++ Back-end/controllers/menuController.js
@@ -1,4 +1,5 @@
 const menuSchema = require('../schemas/menuSchema')
+const pratoSchema = require('../schemas/pratoSchema')
 const { ObjectId } = require("mongodb")
 const fieldCharVerify = require("../services/fieldCharVerify")
 
@@ -55,12 +56,60 @@ module.exports = class{
             if(await fieldCharVerify(diaSemana))
                 return res.status(401).json({message: "Os campos não podem conter caracteres especiais"})
 
+            const acomps = []
             const menu = await menuSchema.findOne({diaSemana:String(diaSemana)})
             if(menu){
-                return res.status(200).json({menu})
+                for(let key in Object.keys(menu.acompanhamentos)){
+                    acomps.push(await pratoSchema.findById(menu.acompanhamentos[key]))
+                }
+                const menuDoc = {
+                    _id: menu._id,
+                    diaSemana: menu.diaSemana,
+                    pratoComum: await pratoSchema.findById(menu.pratoComum),
+                    pratoVegetariano: await pratoSchema.findById(menu.pratoVegetariano),
+                    acompanhamentos: acomps
+                }
+
+                return res.status(200).json({message: menuDoc})
             }
         }catch(err){
-            
+            return res.status(500).json({message: err.message})
+        }
+    }
+
+    static async updateMenu(req, res){
+        try{
+            /* 
+            #swagger.tags = ['Cardápio']
+            #swagger.security = [{
+            "bearerAuth": []
+            }] */
+
+            if(res.user.userCargo === 'user')
+                return res.status(401).json({message: 'Unauthorized'})
+
+            const {diaSemana} = req.params
+            const {pratoComum, pratoVegetariano} = req.body
+            const acompanhamentos = req.body.acompanhamentos?.replace(/[\[\]\s]/g, "").split(',')
+
+            if(await fieldCharVerify(diaSemana))
+                return res.status(401).json({message: "Os campos não podem conter caracteres especiais"})
+
+            if(!diaSemana)
+                return res.status(400).json({message: 'Dia inválido'})
+
+            const menu = await menuSchema.findOne({diaSemana:diaSemana})
+            const menuUpdate = await menuSchema.findOneAndUpdate({diaSemana: diaSemana}, {
+                pratoComum: pratoComum ? pratoComum:menu.pratoComum,
+                pratoVegetariano: pratoVegetariano ? pratoVegetariano:menu.pratoVegetariano,
+                acompanhamentos: acompanhamentos ? acompanhamentos:menu.acompanhamentos
+            })
+
+            if(menuUpdate)
+                return res.status(200).json({message: 'Dados inseridos com sucesso.', menuUpdate})
+            return res.status(400).json({message: 'Houve um erro na requisição.'})
+        }catch(err){
+            return res.status(500).json({message: err.message})
         }
     }
 }
--- Back-end/routes/menuRoutes.js
+++ Back-end/routes/menuRoutes.js
@@ -3,7 +3,8 @@ const router = Router()
 const verifyJWT = require('../services/verifyJWT')
 const menuController = require('../controllers/menuController')
 
-router.post('/menu', verifyJWT, menuController.newMenu)
+// router.post('/menu', verifyJWT, menuController.newMenu)
 router.get('/menu/:diaSemana', menuController.getMenu)
+router.put('/menu/:diaSemana', verifyJWT, menuController.updateMenu)
 
 module.exports = router
